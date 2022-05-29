1b283c85c7a49dbedd8e412ac5089c2b63c9a72f
chanavigail <avigailchan02@gmail.com>
2022-05-29 23:36:31 +0800

    made skeleton for app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+
 import Navbar from "./components/Navbar";
+import MainPage from "./components/MainPage";
+
+import "./index.css";
 
 export default function App() {
     return (
         <div className="App">
             <Navbar />
+            <main>
+                <MainPage />
+            </main>
         </div>
     );
 }
---
+++ my-app/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainPage() {
+    return (
+        <div>
+            <div id="checkin-box">
+                <h1>Check in/out</h1>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
