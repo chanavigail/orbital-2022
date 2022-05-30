f8bba323dafb53fdaf2e0abe0524e6a1eb22a698
chanavigail <avigailchan02@gmail.com>
2022-05-30 16:52:12 +0800

    playing with auth

--- my-app/src/components/Profile.js
+++ my-app/src/components/Profile.js
@@ -1,65 +1,54 @@
-import React from 'react'
-import { supabase } from './db'
+import React from 'react';
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from '../contexts/Auth';
 
 export default function Profile() {
-  const [loading, setLoading] = React.useState(false);
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   const { signUp } = useAuth();
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault()
 
     try {
-      setLoading(true)
       const { error } = await signUp({ email, password })
       if (error) throw error
       alert('error signing in')
     } catch (error) {
       alert(error.error_description || error.message)
     } finally {
-      setLoading(false);
-      history.push("/");
+      navigate("/");
     }
   }
 
   return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget" aria-live="polite">
-        <h1 className="header">Login/Register</h1>
-        {loading ? (
-          'loading...'
-        ) : (
-          <form onSubmit={handleLogin}>
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              className="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              className="inputField"
-              type="password"
-              placeholder="Your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button className="button block">
-              Login
-            </button>
-            <button className="button block">
-              Register
-            </button>
-          </form>
-        )}
-      </div>
+    <div className="profile">
+      <h1 className="header">SignUp</h1>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          className="inputField"
+          type="email"
+          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          className="inputField"
+          type="password"
+          placeholder="Your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="button block">
+          Register
+        </button>
+      </form>
     </div>
   )
 }
--- my-app/src/contexts/Auth.js
+++ my-app/src/contexts/Auth.js
@@ -1,44 +1,40 @@
 import React, { useContext, useState, useEffect } from "react";
-import { supabase } from "../components/db";
+import { supabase } from "../db";
 
 const AuthContext = React.createContext();
-
-export function AuthProvider({ children }) {
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState();
-    
-    useEffect(() => {
-        const session = supabase.auth.session();
-
-        setUser(session?.user ?? null)
-        setLoading(false);
-
-        const { data: listener } = supabase.auth.onAuthStateChange(
-            async (event, session) => {
-                setUser(session?.user ?? null);
-                setLoading(false);
-            }
-        )
-
-        return () => {
-            listener?.unsubscribe()
-        }
-    }, []);
-
-    const value = {
-        signUp: (data) => supabase.auth.signUp(data),
-        signIn: (data) => supabase.auth.signIn(data),
-        signOut: () => supabase.auth.signOut(),
-        user,
-    }
-
-    return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
-}
 
 export function useAuth() {
     return useContext(AuthContext);
 }
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState()
+  
+    useEffect(() => {
+      const session = supabase.auth.session()
+  
+      setUser(session?.user ?? null)
+  
+      const { data: listener } = supabase.auth.onAuthStateChange(
+        async (event, session) => {
+          setUser(session?.user ?? null)
+        }
+      )
+  
+      const ls = () => {listener?.unsubscribe()};
+      return ls;
+    }, []);
+  
+    const value = {
+      signUp: (data) => supabase.auth.signUp(data),
+      signIn: (data) => supabase.auth.signIn(data),
+      signOut: () => supabase.auth.signOut(),
+      user,
+    };
+  
+    return (
+      <AuthContext.Provider value={value}>
+        {children}
+      </AuthContext.Provider>
+    )
+  }
