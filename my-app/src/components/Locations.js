a9f6cc32f7f3656363c523c17f13d94aa0649713
chanavigail <avigailchan02@gmail.com>
2022-05-30 23:08:37 +0800

    making changes to locations page

--- my-app/src/components/Locations.js
+++ my-app/src/components/Locations.js
@@ -5,6 +5,7 @@ function Locations() {
         <div>
             <div id="locations">
                 <h1>All locations</h1>
+                <img src={require("../images/captdh.jpeg")} width="500" height="500" alt="DineTogether logo" className="capt-dh" />
             </div>
         </div>
     );
