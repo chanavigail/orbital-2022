7297ab038b53089d98d82e6e8064caecf37fcf4d
chanavigail <avigailchan02@gmail.com>
2022-06-07 02:11:36 +0800

    trying out something new

--- my-app/src/components/Profile.js
+++ my-app/src/components/Profile.js
@@ -4,8 +4,8 @@ import { useNavigate, Link } from "react-router-dom";
 import { supabase } from '../supabase';
 
 export default function Profile() {
-  // const [email, setEmail] = React.useState('');
-  // const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
   // const { signUp } = useAuth();
   // const navigate = useNavigate();
@@ -20,17 +20,25 @@ export default function Profile() {
   //     navigate("/welcome")
   //   }
   // }
-  async function signInWithGoogle() {
-    const { user, session, error } = await supabase.auth.signIn({
-      provider: 'google',
-    })
+  // async function signInWithGoogle() {
+  //   const { user, session, error } = await supabase.auth.signIn({
+  //     provider: 'google',
+  //   })
+  // }
+
+  async function signUp() {
+    const { data, error } = await supabase
+      .from("users")
+      .insert([
+        { username: email, password: password }
+      ])
   }
 
   return (
     <div className="profile">
       <h1 className="header">Sign Up</h1>
       <form onSubmit={"/welcome"}>
-        {/* <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input
           id="email"
           className="inputField"
@@ -47,11 +55,12 @@ export default function Profile() {
           placeholder="Your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        /> */}
-        <button className="button block" type='submit' onClick={signInWithGoogle}>
+        />
+        <button className="button block" type='submit' onClick={signUp}>
           Register
         </button>
       </form>
+      <p className='profile-message'>Already registered? Click here</p>
     </div>
   )
 }
