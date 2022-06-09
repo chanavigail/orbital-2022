73b833d0c4a81ebd69000b5560ec7c66b7cb3ce6
unknown <e0773279@u.nus.edu>
2022-06-09 17:54:01 +0800

    edited invitations page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,21 +7,21 @@ import MainPage from "./components/MainPage";
 import Locations from "./components/Locations";
 import Friends from "./components/Friends";
 import Profile from "./components/Profile";
-import Invitations from "./components/Invitations";
+import Invitations from "./components/InvitationsPage/Invitations";
 
 import "./index.css";
 
 export default function App() {
-    return (
-        <Router className="App">
-            <Navbar />
-            <Routes>
-                <Route path="/welcome" element={<MainPage />} />
-                <Route path="/locations" element={<Locations />} />
-                <Route path="/friends" element={<Friends />} />
-                <Route path="/invitations" element={<Invitations />} />
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/welcome" element={<MainPage />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/invitations" element={<Invitations />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
--- my-app/src/components/Friends.js
+++ my-app/src/components/Friends.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function Friends() {
-    return (
-        <div>
-            <div id="checkin-box">
-                <h1>Friends</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div id="checkin-box">
+        <h1>Friends</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Friends;
