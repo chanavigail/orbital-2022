6e1abc112f5a0e9f1e1eab9b0642dab6570dc99d
chanavigail <86704275+chanavigail@users.noreply.github.com>
2022-06-24 01:03:40 +0800

    Update helper.js

--- my-app/src/components/helper.js
+++ my-app/src/components/helper.js
@@ -3,3 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://jeuesuvhtlfqygkozslr.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpldWVzdXZodGxmcXlna296c2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ3NjkzMjgsImV4cCI6MTk3MDM0NTMyOH0.po8D4EmWkpkS_yvv9WRQo5-vnC-9ywEPdLFHgn3ez-4";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
