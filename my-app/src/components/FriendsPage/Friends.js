0c006720a3e02329450ebf99d8106db0f4b52652
chanavigail <avigailchan02@gmail.com>
2022-06-27 17:01:49 +0800

    added friendlist import

--- my-app/src/components/FriendsPage/Friends.js
+++ my-app/src/components/FriendsPage/Friends.js
@@ -1,6 +1,8 @@
 import { Stack, Typography } from "@mui/material";
 import React from "react";
 
+import FriendList from "./FriendList";
+
 function Friends() {
   return (
     <Stack spacing={5} sx={{ ml: 2, mr: 2 }}>
