838668c3b13b4091e561187717f044516f98e416
chanavigail <avigailchan02@gmail.com>
2022-06-29 00:11:08 +0800

    working friends backend

--- my-app/src/components/FriendsPage/AddFriend.js
+++ my-app/src/components/FriendsPage/AddFriend.js
@@ -3,17 +3,59 @@ import React, { useEffect } from "react";
 import { supabase } from "../helper";
 
 function AddFriend() {
-    const [ adding, setAdding ] = React.useEffect("");
+    const [ loading, setLoading ] = React.useState(false);
+    const [ addingUser, setAddingUsername ] = React.useEffect("");
+    const [ friendId, setFriendId ] = React.useEffect("");
+    const user = supabase.auth.user()
+
+    async function getId() {
+        const { data: getFriendId } = supabase
+            .from("profiles")
+            .select("id")
+            .match( {username: addingUsername} )
+        setFriendId(getFriendId);
+    }
+
+    const handleAdd = async (e) => {
+        e.preventDefault();
+    
+        try {
+          setLoading(true);
+          const { error } = await supabase
+            .from("friends")
+            .upsert({
+                user_id: user.id,
+                friend_id: friendId
+            })
+          if (error) throw error;
+          alert("You have successsfully added " + adding + " as friend!");
+        } catch (error) {
+          alert(error.error_description || error.message);
+        } finally {
+          setLoading(false);
+        }
+      };
 
     return (
-        <TextField
-            id="friend-search"
-            className="search"
-            type="text"
-            placeholder="input friend's username here"
-            value={adding}
-            onChange={(e) => setAdding(e.target.value)}
-        />
+        <Box
+            component="form"
+            onSubmit={handleAdd}
+            margin="auto"
+        >
+                <TextField
+                    id="friend-search"
+                    className="search"
+                    type="text"
+                    placeholder="input friend's username here"
+                    value={adding}
+                    onChange={(e) => setAddingUsername(e.target.value)}
+                />
+                <Button
+                    variant="contained"
+                    style={{ backgroundColor: "#ffb24d" }}
+                    type="submit"
+                />
+        </Box>
     )
 }
 
