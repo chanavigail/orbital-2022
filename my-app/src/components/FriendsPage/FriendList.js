f524ef78dabb07166e163a2476b9f1444028b6b5
chanavigail <86704275+chanavigail@users.noreply.github.com>
2022-07-26 15:00:19 +0800

    Update FriendList.js

--- my-app/src/components/FriendsPage/FriendList.js
+++ my-app/src/components/FriendsPage/FriendList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { supabase } from "../helper";
-import "./FriendList.css";
+// import "./FriendList.css";
 
 function FriendList() {
   const [ ids, setIds ] = useState([]);
